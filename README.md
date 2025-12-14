7d9597d7a34c9c4c40edf9fab7464cb15ee2231b
bot4o <boevaleksandar@gmail.com>
2025-12-14 13:32:34 +0200

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-Install this plugin manager:
-https://github.com/tmux-plugins/tpm
+This directory includes my `.tmux.conf` and project session script for tmux which you can alter as you like
+
+## Installation
+1. Install tmux plugin manager (TPM):
+```bash
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm 
+```
+
+2. Clone this repo:
+```bash
+git clone https://github.com/bot4o/tmux-config &&
+cd tmux-config
+```
+
+3. Copy the config file to home directory
+```bash
+cp ./.tmux.config ~/.tmux.config
+```
+
+4. Open a empty tmux session (Or just attach to existing one)
+```bash
+tmux 
+```
+or
+```bash
+tmux a
+```
+5. Source the config file
+```bash 
+tmux source ~/.tmux.confg
+```
+6. Enter the "Ctrl+B" and then "I" to install the plugins
+
+For more info check: 
+- https://github.com/tmux-plugins/tpm?tab=readme-ov-file#readme
+
--- dev-app.sh
+++ dev-app.sh
@@ -1,38 +1,43 @@
 #!/bin/bash
-SESH="portfolio"
+# Session name
+SESH="Session name"
 
 tmux has-session -t $SESH 2>/dev/null
-
+# Check for already ran session like this
 if [ $? != 0 ]; then
+    # Editor
     tmux new-session -d -s $SESH -n "editor"
-    
-    tmux send-keys -t $SESH:editor "cd ~/coding/web/Portfolio" C-m
+    tmux send-keys -t $SESH:editor "cd ~/personal/dev/name-of-the-project" C-m
     tmux send-keys -t $SESH:editor "nvim ." C-m
 
+    # Server
     tmux new-window -t $SESH -n "server"
-    tmux send-keys -t $SESH:server "cd ~/coding/web/Portfolio" C-m
+    tmux send-keys -t $SESH:server "cd ~/personal/dev/name-of-the-project" C-m
     tmux send-keys -t $SESH:server "dotnet run" C-m
 
+    # Docker
     tmux new-window -t $SESH -n "docker"
     tmux send-keys -t $SESH:docker "sudo docker run -t backend" C-m
-    
+   
+    # Database
     tmux new-window -t $SESH -n "database"
     tmux send-keys -t $SESH:database "sudo mysql" C-m
-
+    
+    # Logs
     tmux new-window -t $SESH -n "logs"
-    tmux send-keys -t $SESH:logs "tail -f ~/coding/web/Portfolio"
-
+    tmux send-keys -t $SESH:logs "tail -f ~/personal/dev/name-of-the-project"
+    
+    # Appearance
     tmux set-option -t $SESH status on
     tmux set-option -t $SESH status-style fg=white,bg=black
     tmux set-option -t $SESH status-left "#[fg=green]Session: #S #[fg=yellow]"
     tmux set-option -t $SESH status-left-length 40
     tmux set-option -t $SESH status-right "#[fg=cyan]%d %b %R"
-
     tmux set-window-option -t $SESH window-status-style fg=cyan,bg=black
     tmux set-window-option -t $SESH:database window-status-style bg=black
     tmux set-window-option -t $SESH window-status-current-style fg=white,bg=black
-
-
+    
+    # Start window
     tmux select-window -t $SESH:editor
 fi
 tmux attach-session -t $SESH
